872391c0990fa2ee3864bcfca3aa2db4d0956d3b
Bobirjon Tohirov <tokhirovbob@gmail.com>
2023-01-07 10:43:52 +0500

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -6,7 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Panic("Error loading .env file")
+	// }
+}
+
 func main() {
+
+	// db := models.Connect()
+	// r := routes.ApiRoutes(db)
+	// r.Run(os.Getenv("SERVER_RUN_PORT"))
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{

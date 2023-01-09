166dde33b30857673db933f0e95ef6c30f2d7e0f
Bobirjon Tohirov <tokhirovbob@gmail.com>
2023-01-09 12:23:20 +0500

    update

--- backend/models/user.model.go
+++ backend/models/user.model.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-	ID        uint   `gorm:"autoIncrement;primarykey"`
-	Name      string `gorm:"type:varchar(255);not null"`
-	Email     string `gorm:"type:varchar(255);uniqueIndex;not null"`
-	Password  string `gorm:"type:varchar(255);not null"`
+	ID        uint   `gorm:"autoIncrement; primarykey"`
+	Name      string `gorm:"type:varchar(255); not null"`
+	Email     string `gorm:"type:varchar(255); uniqueIndex; not null"`
+	Password  string `gorm:"type:varchar(255); not null"`
 	RoleId    int    `gorm:"default:2"`
-	Status    bool   `gorm:"type:bool;default:true"`
+	Status    bool   `gorm:"type:bool; default:true"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }

5165307fbaf983f8d9e367db81a041da7bb1fbf4
Bobirjon Tohirov <tokhirovbob@gmail.com>
2023-02-22 11:43:22 +0500

    change bearer token

--- backend/middlewares/auth.middleware.go
+++ backend/middlewares/auth.middleware.go
@@ -7,12 +7,22 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
-		if tokenString == "" {
+
+		bearerToken := c.GetHeader("Authorization")
+		if bearerToken == "" {
 			c.JSON(401, gin.H{"error": "Token is empty"})
 			c.Abort()
 			return
 		}
+
+		if len(bearerToken) < 7 || bearerToken[0:7] != "Bearer " {
+			c.JSON(401, gin.H{"error": "Token is not valid"})
+			c.Abort()
+			return
+		}
+
+		tokenString := bearerToken[7:]
+
 		err := services.ValidateToken(tokenString)
 		if err != nil {
 			c.JSON(401, gin.H{"error": err.Error()})

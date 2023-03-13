8712909e0503d8363c75459bfdf82a7d50399af8
Bobirjon Tohirov <tokhirovbob@gmail.com>
2023-03-13 12:04:22 +0500

    delete files vue

--- frontend/src/components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <n-layout-footer bordered class="absolute bottom-0 w-full h-9 items-center flex">
-        <p class="font-semibold text-gray-600 ml-4">
-            Avtomatlashtirilgan bank tizimlarini tadbik etish boʻlimi © 2023
-        </p>
-    </n-layout-footer>
-</template>
